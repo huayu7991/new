6ae7355418799a022b58298cb7c16c9e04758259
root <root@localhost.localdomain>
2018-07-03 16:51:08 +0800

    提交 Test.php

---
+++ web/application/index/controller/Test.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\index\controller;
+//测试单例模式
+class Test
+
+ {
+
+      private static $obj =null;
+
+      private function __construct(){
+       
+      
+      }
+
+      public static function single(){
+        
+        if(empty(self::$obj)){
+           
+           self::$obj = new self();
+        }
+         
+           return self::$obj;
+      }
+}
+
+  $obj =Test::single();
