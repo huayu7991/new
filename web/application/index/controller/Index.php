1e377081ca19547a2ff513fe78f6502fcc17d3cd
huayu7991 <934011212@qq.com>
2018-07-09 15:07:30 +0800

    Update Index.php

--- web/application/index/controller/Index.php
+++ web/application/index/controller/Index.php
@@ -16,31 +16,5 @@ class Index
     {
         return 'hello,' . $name;
     }
-    //curl方式提交表单
-    public function ct(){
-        $url="http://localhost/index/index/testct";
-        $postData=[
-             'title'=>'使用curl方式发送的标题',
-             'content'=>'使用curl方式发送的内容',
-        ];
-        //初始化
- 2      $curl = curl_init();
- 3      //设置抓取的url
- 4      curl_setopt($curl, CURLOPT_URL, $url);
- 5      //设置数据发送方式
- 6      curl_setopt($curl, CURLOPT_POST, 1);
-        //发送数据 
-        curl_setopt($curl,CURLOPT_POSTFIELDS,$postData);
- 7      //设置获取的信息以文件流的形式返回，而不是直接输出。
- 8      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
- 9      //执行命令
-10      $data = curl_exec($curl);
-11      //关闭URL请求
-12      curl_close($curl);
-13      //显示获得的数据
-14      print_r($data);
-    }
-    public function testct(){
-       
-    }
+ 
 }
